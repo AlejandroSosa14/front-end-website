ee57b65ec7b06691a4f441e63ffced245a94e296
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2025-03-11 00:03:04 -0600

    Se actualiza nombre de archivo y se hacen ajustes para la busqueda desde Home.jsx y CarDetails.jsx

---
+++ src/components/search/FormSearch.jsx
@@ -0,0 +1,75 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import SearchCarContext from "../../context/SearchCarContext";
+
+import styles from "./FormSearch.module.css";
+
+const FormSearch = () => {
+	const { locations, brands } = useContext(SearchCarContext);
+	const [brand, setBrand] = useState("");
+	const [locationCity, setLocationCity] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmitSearch = (e) => {
+		e.preventDefault();
+
+		const queryParams = new URLSearchParams();
+		if (brand) queryParams.append("brand", brand);
+		if (locationCity) queryParams.append("locationCity", locationCity);
+		navigate(`/detalle-autos?${queryParams.toString()}`);
+		setBrand("");
+		setLocationCity("");
+	};
+
+	return (
+		<form className={styles.searchForm} onSubmit={handleSubmitSearch}>
+			<div className={styles.searchFormGroup}>
+				<div className={styles.searchFormGroupSelect}>
+					<img className={styles.searchFormSelectIcon} src="/car_icon.svg" alt="Car icon" />
+					<select
+						id="brand"
+						value={brand}
+						onChange={(e) => setBrand(e.target.value)}
+						className={styles.searchFormSelect}
+						required>
+						<option value="" disabled>
+							Marca
+						</option>
+						{brands.map((brand, index) => (
+							<option key={index} value={brand}>
+								{brand}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className={styles.searchFormGroupSelect}>
+					<img
+						className={styles.searchFormSelectIcon}
+						src="/location_icon.svg"
+						alt="Location icon"
+					/>
+					<select
+						id="location"
+						value={locationCity}
+						onChange={(e) => setLocationCity(e.target.value)}
+						className={styles.searchFormSelect}
+						required>
+						<option value="" disabled>
+							Ubicación
+						</option>
+						{locations.map((location, index) => (
+							<option key={index} value={location}>
+								{location}
+							</option>
+						))}
+					</select>
+				</div>
+			</div>
+			<button className="main-btn" type="submit">
+				Buscar
+			</button>
+		</form>
+	);
+};
+
+export default FormSearch;
