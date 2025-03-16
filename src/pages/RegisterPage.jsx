64992de6cb24475127c696c0e63b2203521d7a9e
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2025-03-15 23:51:50 -0600

    Ajuste de uso de clases para evitar conflictos en estilos de los botones

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,36 +4,34 @@ import Layout from "../components/layout/Layout";
 import styles from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
-  const formRef = useRef(null);
+	const formRef = useRef(null);
 
-  const handleSubmit = () => {
-    if (formRef.current) {
-      formRef.current.dispatchEvent(
-        new Event("submit", { cancelable: true, bubbles: true })
-      );
-    }
-  };
+	const handleSubmit = () => {
+		if (formRef.current) {
+			formRef.current.dispatchEvent(new Event("submit", { cancelable: true, bubbles: true }));
+		}
+	};
 
-  return (
-    <Layout>
-      <section className={styles.register}>
-        <h2>Crear Cuenta</h2>
-        <div className="container">
-          <div className={styles.registerContainer}>
-            <div className={styles.registerFormContainer}>
-              <RegisterForm onFormReady={formRef} />
-            </div>
-          </div>
-        </div>
+	return (
+		<Layout>
+			<section className={styles.register}>
+				<h2>Crear Cuenta</h2>
+				<div className="container">
+					<div className={styles.registerContainer}>
+						<div className={styles.registerFormContainer}>
+							<RegisterForm onFormReady={formRef} />
+						</div>
+					</div>
+				</div>
 
-        <div className={styles.buttonContainer}>
-          <button className={styles.submitButton} type="button" onClick={handleSubmit}>
-            Crear Cuenta
-          </button>
-        </div>
-      </section>
-    </Layout>
-  );
+				<div className={styles.buttonContainer}>
+					<button className="main-btn" type="button" onClick={handleSubmit}>
+						Crear Cuenta
+					</button>
+				</div>
+			</section>
+		</Layout>
+	);
 };
 
 export default RegisterPage;
